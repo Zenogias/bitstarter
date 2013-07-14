d2ff2da4d1d03f61ddb4b021d4f1fecc16941010
Zenogias <Zeno1Gias@gmail.com>
2013-07-14 15:47:07 +0000

    Going back on web.js trying the original

--- web.js
+++ web.js
@@ -3,9 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var buffer = new Buffer();
-    response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
+  response.send('Hello World 3 I guess?');
 });
 
 var port = process.env.PORT || 5000;
